ba0917a92f9487c986ad0e2cac0842f3912a69e9
Steven Prindle <flankbusiness@gmail.com>
2020-02-23 11:23:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,17 +1,13 @@
 def key_for_min_value(name_hash)
 
-  i = 0
-  min_val = nil
-  min_key = nil
-  name_hash.each do |key, val|
-    if i == 0
-      min_val = val
-      min_key = key
-    elsif val < min_val
-      min_val = val
-      min_key = key
-    end
-    i += 1
-  end
-  min_key
 end 
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.collect do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
